f42be310e6d81c8926a1cefc1c5359306229fda8
MCappe <melissacappe@gmail.com>
2020-11-13 16:09:49 -0500

    got it working

--- styles/javascript/script.js
+++ styles/javascript/script.js
@@ -1,10 +1,3 @@
-// wait for user to click on 'enter game' - create an event listener to bring up the first question
-// answer options will be clickable - event listener so that when the user selects and answer, they will be told if correct or incorrect (if incorrect, the correct answer will show)
-// loop through for 5 questions
-// upon completion, add an event listener to to the final question so that user will see their score on the screen
-// play again button will show up below score
-// event listener if they click play again - resets the page
-
 // Namespacing
 const triviaApp = {};
 
@@ -14,7 +7,10 @@ triviaApp.init = () => {
 }
 
 triviaApp.questionCount = 0;
+triviaApp.score = 0;
+triviaApp.userGuess = '';
 
+// Question/Answer array
 triviaApp.triviaQuestions = [
     {
         question: 'How many films did Sean Connery play James Bond in?',
@@ -22,29 +18,9 @@ triviaApp.triviaQuestions = [
         correctAnswer: '7'
     },
     {
-        question: 'What product is seen in every scene in Fight Club?',
-        answers: ['Starbucks', 'Coca-cola', 'Nestle'],
-        correctAnswer: 'Starbucks'
-    },
-    {
-        question: 'What was the first movie in the Marvel Cinematic Universe?',
-        answers: ['Avengers', 'Iron Man', 'Captain America: Civil War'],
-        correctAnswer: 'Iron Man'
-    },
-    {
         question: 'What city is the TV show The Wire set in?',
         answers: ['Boston', 'Baltimore', 'Chicago'],
         correctAnswer: 'Baltimore'
-    },
-    {
-        question: 'What concert did Monica, Ross and Chandler attend on the show Friends?',
-        answers: ['Counting Crows', 'Gin Blossoms', 'Hootie and the Blowfish'],
-        correctAnswer: 'Hootie and the Blowfish'
-    },
-    {   
-        question: 'What is the hashtag symbol technically called?',
-        answers: ['Pound sign', 'Octothorp', 'Hectothorp'],
-        correctAnswer: 'Octothorp'
     },
     {
         question: 'What was bubble wrap originally used for?',
@@ -55,11 +31,6 @@ triviaApp.triviaQuestions = [
         question: 'What was the first muscle car?',
         answers: ['Pontiac GTO', 'Dodge Charger', 'Ford Mustang'],
         correctAnswer: 'Pontiac GTO'
-    },
-    {
-        question: 'What was the first toy ever to be advertised on television',
-        answers: ['Barbie', 'Mr. Potato Head', 'G.I. Joe'],
-        correctAnswer: 'Mr. Potato Head'
     },
     {
         question: 'What colour are aircraft black boxes?',
@@ -76,31 +47,50 @@ triviaApp.eventListener = () => {
     // event listener hide the 'enter game' button once clicked
     $('#enter').on('click', function () {
         $('.game-entrance').hide();
+
         // event listener to display the first question (and possible answers) on the page
-        
-        console.log(triviaApp.triviaQuestions);
+    
         $('#questions').html(`<h2>${triviaApp.triviaQuestions[0].question}</h2>`);
-        $('#answers').html(`<button>${triviaApp.triviaQuestions[0].answers[0]}</button>
-        <button>${triviaApp.triviaQuestions[0].answers[1]}</button>
-        <button>${triviaApp.triviaQuestions[0].answers[2]}</button>`);
+        $('#answers').html(`
+        <button data-q="1" data-answerVal="6">${triviaApp.triviaQuestions[0].answers[0]}</button>
+        <button data-q="1" data-answerVal="7">${triviaApp.triviaQuestions[0].answers[1]}</button>
+        <button data-q="1" data-answerVal="5">${triviaApp.triviaQuestions[0].answers[2]}</button>
+        `);
+        
+        // create an event listener that listens for user to click on an answer button, which will prompt the next question to come up on the page
         
     });
+        $('#answers').on('click', 'button', function () {
+            // 1) tally up the points - check user's answer
+            // 2) move to the next question in the array
+            console.log(triviaApp.questionCount)
+            if (triviaApp.questionCount == 4) {
+                console.log('results')
+                $('.results').html(`<h2>You scored ${triviaApp.score} out of 5</h2>`)
+                
+            } else {
+                const chosenAnswer = $(this).attr('data-answerVal');
+                const correct = triviaApp.triviaQuestions[triviaApp.questionCount].correctAnswer;
+                if (correct === chosenAnswer) {
+                    triviaApp.score++;
+                    
+                } 
+                triviaApp.questionCount++;
+                $('#questions').html(`<h2>${triviaApp.triviaQuestions[triviaApp.questionCount].question}</h2>`);
+                $('#answers').html(`
+                <button data-q="1" data-answerVal="6">${triviaApp.triviaQuestions[triviaApp.questionCount].answers[0]}</button>
+                <button data-q="1" data-answerVal="7">${triviaApp.triviaQuestions[triviaApp.questionCount].answers[1]}</button>
+                <button data-q="1" data-answerVal="5">${triviaApp.triviaQuestions[triviaApp.questionCount].answers[2]}</button>
+            `);
+
+            }
+            
+        })
+        
 }
 
-// randomly pick question from array
-// display question and all possibly answers
-
-
-
-
-
-
-
-// click function to select answer
-$('#answers').on('click', function() {
-})
-
-
+// create an event listener to listen for when the user clicks the final answer, which will prompt a results <h2>
+// unhide the #replay button
 
 $(function () {
     triviaApp.init();
